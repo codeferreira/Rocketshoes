835950362afac418f9e10b2463544cb95abae7cd
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-10-20 23:56:01 -0300

    feat: Get products from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,101 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get('/products');
+
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
+
+      setProducts(data);
+    }
+
+    getProducts();
+  }, []);
+
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/8f31b7a8e7f34c9085d1a80f009b2cc9_9366/Tenis_Ultraboost_Preto_BB6166_01_standard.jpg"
-          alt="Tênis"
-        />
-        <strong>Adidas Ultraboost</strong>
-        <span>$150.00</span>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/8f31b7a8e7f34c9085d1a80f009b2cc9_9366/Tenis_Ultraboost_Preto_BB6166_01_standard.jpg"
-          alt="Tênis"
-        />
-        <strong>Adidas Ultraboost</strong>
-        <span>$150.00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/8f31b7a8e7f34c9085d1a80f009b2cc9_9366/Tenis_Ultraboost_Preto_BB6166_01_standard.jpg"
-          alt="Tênis"
-        />
-        <strong>Adidas Ultraboost</strong>
-        <span>$150.00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/8f31b7a8e7f34c9085d1a80f009b2cc9_9366/Tenis_Ultraboost_Preto_BB6166_01_standard.jpg"
-          alt="Tênis"
-        />
-        <strong>Adidas Ultraboost</strong>
-        <span>$150.00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/8f31b7a8e7f34c9085d1a80f009b2cc9_9366/Tenis_Ultraboost_Preto_BB6166_01_standard.jpg"
-          alt="Tênis"
-        />
-        <strong>Adidas Ultraboost</strong>
-        <span>$150.00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/8f31b7a8e7f34c9085d1a80f009b2cc9_9366/Tenis_Ultraboost_Preto_BB6166_01_standard.jpg"
-          alt="Tênis"
-        />
-        <strong>Adidas Ultraboost</strong>
-        <span>$150.00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Add to Cart</span>
-        </button>
-      </li>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
+            <span>Add to Cart</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 }
