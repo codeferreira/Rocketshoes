68b7b2dc4832e0d9cb77571f68415185ccf428b3
José JC. Carlos <dev.josejunior@gmail.com>
2019-10-24 18:16:16 -0300

    feat: Add verification of stock amount before adding to cart

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,18 +1,30 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
 
-import { addToCartSucess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
   const productExists = yield select(state =>
     state.cart.find(p => p.id === id)
   );
 
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    toast.error('Order amount unavailable');
+    return;
+  }
+
   if (productExists) {
-    const amount = productExists.amount + 1;
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -22,8 +34,28 @@ function* addToCart({ id }) {
       priceFormatted: formatPrice(response.data.price),
     };
 
-    yield put(addToCartSucess(data));
+    yield put(addToCartSuccess(data));
+
+    toast.success('Added to Cart!');
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Order amount unavailable');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
